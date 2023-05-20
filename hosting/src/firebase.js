10d67dbb086c367926c7b35f3358a37f45c9050c
himanshu22500 <78587168+himanshu22500@users.noreply.github.com>
2023-05-20 22:45:37 +0530

    Update firebase.js

--- hosting/src/firebase.js
+++ hosting/src/firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD79aeAo9-T0KhufQh4E-_yhoWuLDFfz8g",
-  authDomain: "split-it-40312.firebaseapp.com",
-  projectId: "split-it-40312",
-  storageBucket: "split-it-40312.appspot.com",
-  messagingSenderId: "511012206927",
-  appId: "1:511012206927:web:69b49b6ed3a8b15b97ad3a",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
